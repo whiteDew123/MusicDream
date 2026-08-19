4a1d92aeb84f759a0b3e0ea8b25231d475bfcf4c
ZJ-123-CPU <2849055560@QQ.com>
2026-08-19 17:05:08 +0800

    feat: 完善Mod_recommend推荐模块，修复MyBatis Plus依赖，注册Nacos服务发现

--- musicBack/Mod_recommend/src/main/java/com/itheima/recommend/RecommendApplication.java
+++ musicBack/Mod_recommend/src/main/java/com/itheima/recommend/RecommendApplication.java
@@ -3,11 +3,13 @@ package com.itheima.recommend;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 歌曲推荐搜索模块微服务启动类
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("com.itheima.recommend.mapper")
 public class RecommendApplication {
 
